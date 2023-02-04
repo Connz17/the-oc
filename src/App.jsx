32d877276329f14424748584da1a40c580c61bf3
Connel Nwaodor <connel_n@hotmail.com>
2023-02-04 20:46:24 +0000

    Landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import './App.scss';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from './containers/HomePage/HomePage';
+import LandingPage from './containers/LandingPage/LandingPage';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/home"
+            element={<HomePage/>}>
+          </Route>
+          <Route path='/'
+            element={<LandingPage/>}>
+          </Route>
+        </Routes>
+      </div>
+    </Router>  
   );
 }
 
--- src/containers/LandingPage/LandingPage.jsx
+++ src/containers/LandingPage/LandingPage.jsx
@@ -1,9 +1,23 @@
 import "./LandingPage.scss";
+import sunflower from "../../Images/pngegg.png"
+import { Link } from "react-router-dom";
 
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <div>
+        <div className="landing-title">
+            <h1>&#128420;</h1>
+            <h1>"Get weird or go home"</h1>
+            <h1>&#128155;</h1>
+        </div>
+        <div>
+            <h2 className="heart-duo">&#128153;&#129505;</h2>
+        </div>
+        <Link to="/home">
+            <img className="landing-button" src={sunflower} alt="entry button" />
+        </Link>
+    </div>
   )
 }
 
