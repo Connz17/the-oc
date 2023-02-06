34973c1064882be80023dc44b88b79445da674ef
Connel Nwaodor <connel_n@hotmail.com>
2023-02-06 16:52:14 +0000

    Home page formatted

--- src/containers/HomePage/HomePage.jsx
+++ src/containers/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
+import verse from "../../Images/verse galaxy.jpg";
+//import verse from "../../Images/verse compass.jpg";
 import "./HomePage.scss";
 
 const HomePage = () => {
   return (
     <div className="home">
-      <h2>
-        Olivia Estella Ann Brown ever since December the 23rd, and even more so
+      <h2 className="home_intro">
+        Olivia Estella Ann Brown. Ever since December the 23rd, and even more so
         since January 12th, you have brightened up my day to no end. You have
         brought joy, laughter, amusement and happiness each and every day. Even
         when you're not trying, you still look gorgeous. There is a quote that
@@ -13,9 +15,13 @@ const HomePage = () => {
         not just for a moment in my life, but for the rest of it...When I think
         about my future, all I see is you in it.
       </h2>
-      <h3>Verse</h3>
+      <br />
+      <div className="home__verse">
+        <img className="verse__image" src={verse} alt=""></img>
+      </div>
+      <br />
       <h1>
-        &#128420; I want you to always be my honeybun...so will you from today
+        &#128420; I want you to always be my honeybun...so from today...will you
         be my girlfriend? &#128155;
       </h1>
     </div>
