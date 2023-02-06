f90aa43e8430fd5a3bdc13c4a31b1a7c9022d851
Connel Nwaodor <connel_n@hotmail.com>
2023-02-06 15:53:35 +0000

    Landing page done

--- src/containers/HomePage/HomePage.jsx
+++ src/containers/HomePage/HomePage.jsx
@@ -1,10 +1,25 @@
 import "./HomePage.scss";
 
-
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div className="home">
+      <h2>
+        Olivia Estella Ann Brown ever since December the 23rd, and even more so
+        since January 12th, you have brightened up my day to no end. You have
+        brought joy, laughter, amusement and happiness each and every day. Even
+        when you're not trying, you still look gorgeous. There is a quote that
+        goes “When you discover something that brings joy, care enough about
+        yourself to make room for it in your life.” I want to make room for you,
+        not just for a moment in my life, but for the rest of it...When I think
+        about my future, all I see is you in it.
+      </h2>
+      <h3>Verse</h3>
+      <h1>
+        &#128420; I want you to always be my honeybun...so will you from today
+        be my girlfriend? &#128155;
+      </h1>
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
